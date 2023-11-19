baf2a2f440d7f1986cb7367dcfafc40da3f405c7
segin-GH <segin.bytes@gmail.com>
2023-11-19 21:30:00 +0530

    feat: Added GET to get the data of port form oats-Agent

--- cereal/script.js
+++ cereal/script.js
@@ -10,6 +10,9 @@ function loadContent(page) {
       document.getElementById('content-area').innerHTML = html;
       // Attach event listener after content is loaded
       attachDropdownListener();
+      if (page === 'usb') {
+        getPortDataFromOatsAgent();
+      }
     })
     .catch(error => {
       console.error('Error loading content:', error);
@@ -22,11 +25,44 @@ function attachDropdownListener() {
   if (dropdownButton) {
     dropdownButton.addEventListener('click', function () {
       console.log("Dropdown button clicked");
+      getPortDataFromOatsAgent();
       document.getElementById('dropdownMenu').classList.toggle('hidden');
     });
   } else {
     console.warn('Dropdown button not found');
   }
 }
+
+function getPortDataFromOatsAgent() {
+  fetch('http://localhost:5000/usb')
+    .then(response => response.json())
+    .then(data => {
+      console.log("Data from oatsAgent:\n\n", JSON.stringify(data, null, 2));
+
+      // Get the dropdown menu element
+      var dropdownMenu = document.getElementById('dropdownMenu');
+
+      // Clear existing menu items
+      dropdownMenu.innerHTML = '';
+
+      // Add new menu items for each port
+      data.ports.forEach(port => {
+        // Extract port name, e.g., "USB0" from "/dev/ttyUSB0"
+        var portName = port.port.match(/tty(USB\d+)/)[1];
+
+        // Create new menu item
+        var menuItem = document.createElement('a');
+        menuItem.href = '#';
+        menuItem.className = 'block p-2 hover:bg-clr-bg-thr';
+        menuItem.textContent = portName;
+
+        // Append new menu item to the dropdown
+        dropdownMenu.appendChild(menuItem);
+      });
+
+    })
+    .catch(error => console.error('Error:', error));
+}
+
 
 loadContent('usb');
