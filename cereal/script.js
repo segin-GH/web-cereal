d15888c2d172a69f9435e784ff19d08ed4f3ddaf
segin-GH <segin.bytes@gmail.com>
2023-11-25 23:54:31 +0530

    feat (usb.html): added dropdown for baudrate in settings

--- cereal/script.js
+++ cereal/script.js
@@ -22,9 +22,55 @@ function loadContent(page) {
 
 function attachDropdownListener() {
   document.getElementById('dropdownUSBPort').addEventListener('click', function () {
-    var dropdown = document.getElementById('dropdownUSB');
+    var dropdownUSB = document.getElementById('dropdownUSB');
+    dropdownUSB.classList.toggle('hidden');
+  });
+
+  // Event listeners for USB port selections
+  var usbPortItems = document.querySelectorAll('#dropdownUSB li');
+  usbPortItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+      var selectedPort = this.textContent;
+      var buttonUSBPort = document.getElementById('dropdownUSBPort');
+      buttonUSBPort.textContent = selectedPort; // Update button text
+      buttonUSBPort.appendChild(createDownArrow()); // Add the down arrow again
+    });
+  });
+
+  document.getElementById('dropdownBaudRate').addEventListener('click', function () {
+    var dropdown = document.getElementById('dropdownBaud');
     dropdown.classList.toggle('hidden');
   });
+
+  var baudRateItems = document.querySelectorAll('#dropdownBaud li');
+  baudRateItems.forEach(function (item) {
+    item.addEventListener('click', function () {
+      var selectedBaudRate = this.textContent;
+      var buttonBaudRate = document.getElementById('dropdownBaudRate');
+      buttonBaudRate.textContent = selectedBaudRate; // Update button text
+      buttonBaudRate.appendChild(createDownArrow()); // Add the down arrow again
+    });
+  });
+
+  function createDownArrow() {
+    var svgNS = "http://www.w3.org/2000/svg";
+    var svg = document.createElementNS(svgNS, "svg");
+    svg.setAttribute("class", "ml-2 w-2 h-2");
+    svg.setAttribute("aria-hidden", "true");
+    svg.setAttribute("fill", "none");
+    svg.setAttribute("viewBox", "0 0 10 6");
+
+    var path = document.createElementNS(svgNS, "path");
+    path.setAttribute("stroke", "currentColor");
+    path.setAttribute("stroke-linecap", "round");
+    path.setAttribute("stroke-linejoin", "round");
+    path.setAttribute("stroke-width", "2");
+    path.setAttribute("d", "m1 1 4 4 4-4");
+
+    svg.appendChild(path);
+    return svg;
+  }
+
 }
 
 function getPortDataFromOatsAgent() {
