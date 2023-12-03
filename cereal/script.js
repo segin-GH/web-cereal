39927ca0d21a9d0de3ab9595c44f6005ce0f4de9
segin-GH <segin.bytes@gmail.com>
2023-12-03 23:34:19 +0530

    fix: clean up and restructured the code

--- cereal/script.js
+++ cereal/script.js
@@ -1,3 +1,10 @@
+
+/**
+ * Main JavaScript file for handling page content loading and interactive elements.
+ * Includes functions for loading HTML content, attaching event listeners, and handling dropdowns.
+ */
+
+// Function to load HTML content into a specified element
 function loadContent(page) {
   fetch(`${page}.html`)
     .then(response => {
@@ -8,8 +15,7 @@ function loadContent(page) {
     })
     .then(html => {
       document.getElementById('content-area').innerHTML = html;
-      // Attach event listener after content is loaded
-      attachDropdownListener();
+      attachEventListeners();
     })
     .catch(error => {
       console.error('Error loading content:', error);
@@ -17,93 +23,64 @@ function loadContent(page) {
     });
 }
 
-function attachDropdownListener() {
-  // Check and attach listener for USB Port dropdown
-  var dropdownUSBPort = document.getElementById('dropdownUSBPort');
-  if (dropdownUSBPort) {
-    dropdownUSBPort.addEventListener('click', function () {
-      var dropdownUSB = document.getElementById('dropdownUSB');
-      if (dropdownUSB) {
-        dropdownUSB.classList.toggle('hidden');
-      }
-    });
-
-    var usbPortItems = document.querySelectorAll('#dropdownUSB li');
-    usbPortItems.forEach(function (item) {
-      item.addEventListener('click', function () {
-        var selectedPort = this.textContent;
-        dropdownUSBPort.textContent = selectedPort;
-        dropdownUSBPort.appendChild(createDownArrow());
-        if (dropdownUSB) {
-          dropdownUSB.classList.add('hidden');
-        }
-      });
-    });
-  }
-
-  // Check and attach listener for Baud Rate dropdown
-  var dropdownBaudRate = document.getElementById('dropdownBaudRate');
-  if (dropdownBaudRate) {
-    dropdownBaudRate.addEventListener('click', function () {
-      var dropdownBaud = document.getElementById('dropdownBaud');
-      if (dropdownBaud) {
-        dropdownBaud.classList.toggle('hidden');
-      }
-    });
-
-    var baudRateItems = document.querySelectorAll('#dropdownBaud li');
-    baudRateItems.forEach(function (item) {
-      item.addEventListener('click', function () {
-        var selectedBaudRate = this.textContent;
-        dropdownBaudRate.textContent = selectedBaudRate;
-        dropdownBaudRate.appendChild(createDownArrow());
-        if (dropdownBaud) {
-          dropdownBaud.classList.add('hidden');
-        }
-      });
-    });
-  }
-  var dropdownLineEnding = document.getElementById('dropdownLineEnding');
-  if (dropdownLineEnding) {
-    dropdownLineEnding.addEventListener('click', function () {
-      toggleDropdown('dropdownLineEnd');
-    });
-
-    var lineEndingItems = document.querySelectorAll('#dropdownLineEnd li');
-    lineEndingItems.forEach(function (item) {
-      item.addEventListener('click', function () {
-        var selectedLineEnding = this.textContent;
-        dropdownLineEnding.textContent = selectedLineEnding;
-        dropdownLineEnding.appendChild(createDownArrow());
-        toggleDropdown('dropdownLineEnd');
-      });
-    });
-  }
-  function createDownArrow() {
-    var svgNS = "http://www.w3.org/2000/svg";
-    var svg = document.createElementNS(svgNS, "svg");
-    svg.setAttribute("class", "ml-2 w-2 h-2");
-    svg.setAttribute("aria-hidden", "true");
-    svg.setAttribute("fill", "none");
-    svg.setAttribute("viewBox", "0 0 10 6");
-
-    var path = document.createElementNS(svgNS, "path");
-    path.setAttribute("stroke", "currentColor");
-    path.setAttribute("stroke-linecap", "round");
-    path.setAttribute("stroke-linejoin", "round");
-    path.setAttribute("stroke-width", "2");
-    path.setAttribute("d", "m1 1 4 4 4-4");
-
-    svg.appendChild(path);
-    return svg;
-  }
-
-  function toggleDropdown(dropdownId) {
-    var dropdown = document.getElementById(dropdownId);
-    dropdown.classList.toggle('hidden');
-  }
-
+// Function to attach event listeners to various elements
+function attachEventListeners() {
+  attachDropdownListener('dropdownUSBPort', 'dropdownUSB');
+  attachDropdownListener('dropdownBaudRate', 'dropdownBaud');
+  attachDropdownListener('dropdownLineEnding', 'dropdownLineEnd');
 }
 
+// Generic function to attach a listener to a dropdown
+function attachDropdownListener(dropdownButtonId, dropdownContentId) {
+  var dropdownButton = document.getElementById(dropdownButtonId);
+  if (dropdownButton) {
+    dropdownButton.addEventListener('click', function () {
+      toggleDropdown(dropdownContentId);
+    });
 
+    var dropdownItems = document.querySelectorAll(`#${dropdownContentId} li`);
+    dropdownItems.forEach(item => {
+      item.addEventListener('click', function () {
+        updateDropdownSelection(dropdownButton, this.textContent);
+        toggleDropdown(dropdownContentId);
+      });
+    });
+  }
+}
+
+// Function to update the dropdown selection display
+function updateDropdownSelection(dropdownButton, selectedValue) {
+  dropdownButton.textContent = selectedValue;
+  dropdownButton.appendChild(createDownArrow());
+}
+
+// Function to create a down arrow SVG element
+function createDownArrow() {
+  var svgNS = "http://www.w3.org/2000/svg";
+  var svg = document.createElementNS(svgNS, "svg");
+  svg.setAttribute("class", "ml-2 w-2 h-2");
+  svg.setAttribute("aria-hidden", "true");
+  svg.setAttribute("fill", "none");
+  svg.setAttribute("viewBox", "0 0 10 6");
+
+  var path = document.createElementNS(svgNS, "path");
+  path.setAttribute("stroke", "currentColor");
+  path.setAttribute("stroke-linecap", "round");
+  path.setAttribute("stroke-linejoin", "round");
+  path.setAttribute("stroke-width", "2");
+  path.setAttribute("d", "m1 1 4 4 4-4");
+
+  svg.appendChild(path);
+  return svg;
+}
+
+// Function to toggle dropdown visibility
+function toggleDropdown(dropdownId) {
+  var dropdown = document.getElementById(dropdownId);
+  if (dropdown) {
+    dropdown.classList.toggle('hidden');
+  }
+}
+
+// Initial content loading
 loadContent('usb');
