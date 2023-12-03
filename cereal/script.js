b715ea32cc0c89e790271e7f5fc2a9d51abdda28
segin-GH <segin.bytes@gmail.com>
2023-12-04 00:55:44 +0530

    fix: clean up and the main entry point is script.js

--- cereal/script.js
+++ cereal/script.js
@@ -1,10 +1,6 @@
+// main.js
+import { attachUsbEventListeners } from './usb.js';
 
-/**
- * Main JavaScript file for handling page content loading and interactive elements.
- * Includes functions for loading HTML content, attaching event listeners, and handling dropdowns.
- */
-
-// Function to load HTML content into a specified element
 function loadContent(page) {
   fetch(`${page}.html`)
     .then(response => {
@@ -15,7 +11,7 @@ function loadContent(page) {
     })
     .then(html => {
       document.getElementById('content-area').innerHTML = html;
-      attachEventListeners();
+      attachUsbEventListeners();
     })
     .catch(error => {
       console.error('Error loading content:', error);
@@ -23,64 +19,15 @@ function loadContent(page) {
     });
 }
 
-// Function to attach event listeners to various elements
-function attachEventListeners() {
-  attachDropdownListener('dropdownUSBPort', 'dropdownUSB');
-  attachDropdownListener('dropdownBaudRate', 'dropdownBaud');
-  attachDropdownListener('dropdownLineEnding', 'dropdownLineEnd');
-}
-
-// Generic function to attach a listener to a dropdown
-function attachDropdownListener(dropdownButtonId, dropdownContentId) {
-  var dropdownButton = document.getElementById(dropdownButtonId);
-  if (dropdownButton) {
-    dropdownButton.addEventListener('click', function () {
-      toggleDropdown(dropdownContentId);
+document.addEventListener('DOMContentLoaded', () => {
+  document.querySelectorAll('.nav-item').forEach(item => {
+    item.addEventListener('click', event => {
+      event.preventDefault();
+      const page = item.getAttribute('data-page');
+      loadContent(page);
     });
-
-    var dropdownItems = document.querySelectorAll(`#${dropdownContentId} li`);
-    dropdownItems.forEach(item => {
-      item.addEventListener('click', function () {
-        updateDropdownSelection(dropdownButton, this.textContent);
-        toggleDropdown(dropdownContentId);
-      });
-    });
-  }
-}
-
-// Function to update the dropdown selection display
-function updateDropdownSelection(dropdownButton, selectedValue) {
-  dropdownButton.textContent = selectedValue;
-  dropdownButton.appendChild(createDownArrow());
-}
-
-// Function to create a down arrow SVG element
-function createDownArrow() {
-  var svgNS = "http://www.w3.org/2000/svg";
-  var svg = document.createElementNS(svgNS, "svg");
-  svg.setAttribute("class", "ml-2 w-2 h-2");
-  svg.setAttribute("aria-hidden", "true");
-  svg.setAttribute("fill", "none");
-  svg.setAttribute("viewBox", "0 0 10 6");
-
-  var path = document.createElementNS(svgNS, "path");
-  path.setAttribute("stroke", "currentColor");
-  path.setAttribute("stroke-linecap", "round");
-  path.setAttribute("stroke-linejoin", "round");
-  path.setAttribute("stroke-width", "2");
-  path.setAttribute("d", "m1 1 4 4 4-4");
-
-  svg.appendChild(path);
-  return svg;
-}
-
-// Function to toggle dropdown visibility
-function toggleDropdown(dropdownId) {
-  var dropdown = document.getElementById(dropdownId);
-  if (dropdown) {
-    dropdown.classList.toggle('hidden');
-  }
-}
+  });
+});
 
 // Initial content loading
 loadContent('usb');
