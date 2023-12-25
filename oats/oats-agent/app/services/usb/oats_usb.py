434483f88bc2df02563f9ce82ed4811fc3170027
segin-GH <segin.bytes@gmail.com>
2023-12-25 19:58:16 +0530

    refactor: split into funcitons and can handle ids

--- oats/oats-agent/app/services/usb/oats_usb.py
+++ oats/oats-agent/app/services/usb/oats_usb.py
@@ -7,40 +7,51 @@ logger = setup_logger(__name__)
 class OatsUSB:
     def __init__(self):
         self.oats_usb_dict = {}
+        self.oats_uid = 0
         logger.debug("OatsUSB instance.")
+
+    def oats_get_uuid(self):
+        self.oats_uid += 1
+        return str(self.oats_uid)
 
     def get_port_data_for_client(self):
         ports = sp.get_available_ports()
         logger.info(f"Available ports: {ports}")
         return ports
 
-    def process_req_from_client(self, conf):
-        conf_id = conf['id']
-        logger.info(f"Processing request for id: {conf_id}")
+    def process_connect_request(self, request):
+        conf_id = request['id']
+        if conf_id == 'tab1':  # TODO"tab1", really? ever heard of meaningful names?
+            conf_id = self.oats_get_uuid()
+
+        logger.info(f"Processing connect request for id: {conf_id}")
         if conf_id not in self.oats_usb_dict:
-            logger.info(f"Creating new SerialPort instance for id: {conf_id}")
             self.oats_usb_dict[conf_id] = sp.SerialPort(
-                conf['port'], conf['baudrate'], conf['endline'])
+                request['port'], request['baudrate'], request['endline'])
+            logger.info(f"Created new SerialPort for id: {conf_id}")
         else:
-            logger.info(
-                f"Updating existing SerialPort instance for id: {conf_id}")
             self.oats_usb_dict[conf_id].update_conf(
-                conf['port'], conf['baudrate'], conf['endline'])
+                request['port'], request['baudrate'], request['endline'])
+            logger.info(f"Updated SerialPort for id: {conf_id}")
 
-        if conf['enabled']:
-            logger.info(f"Enabling SerialPort for id: {conf_id}")
-            self.oats_usb_dict[conf_id].turn_on()
-        else:
-            logger.info(f"Disabling SerialPort for id: {conf_id}")
-            self.oats_usb_dict[conf_id].turn_off()
+        self.oats_usb_dict[conf_id].turn_on()
+        logger.info(f"Enabled SerialPort for id: {conf_id}")
+        return self.oats_usb_dict[conf_id].get_conf_json(), conf_id
 
-        conf_json = self.oats_usb_dict[conf_id].get_conf_json()
-        logger.debug(f"Configuration for id {conf_id}:\n {conf_json}")
-        return conf_json
+    def process_disconnect_request(self, request):
+        conf_id = request['id']
+        if conf_id not in self.oats_usb_dict:
+            # Nice error handling
+            raise ValueError(
+                "Trying to disconnect a non-existent connection. Good job.")
 
-    def send_data_to_serial(self, data, id='tab1'):
+        self.oats_usb_dict[conf_id].turn_off()
+        logger.info(f"Disabled SerialPort for id: {conf_id}")
+        return self.oats_usb_dict[conf_id].get_conf_json(), conf_id
+
+    def send_data_to_serial(self, data, id="1"):
         if id in self.oats_usb_dict:
-            self.oats_usb_dict['tab1'].write_data(data)
+            self.oats_usb_dict[id].write_data(data)
         else:
             logger.warning(
                 "Attempted to send data to serial, but 'tab1' is not in oats_usb_dict")
