0b0a63264de05e2517e7303c38be05d3db9872b7
segin-GH <segin.bytes@gmail.com>
2023-12-29 06:17:49 +0530

    fix: logs printing twice?

--- oats/oats-agent/app/__init__.py
+++ oats/oats-agent/app/__init__.py
@@ -4,39 +4,17 @@ from flask_cors import CORS
 from flask_socketio import SocketIO
 
 socketio = SocketIO()
-logger = setup_logger(__name__)
 
 
 def create_app():
     app = Flask(__name__)
-
-    logger.info("Starting Flask app")
-
     CORS(app, cors_allowed_origins="*")
     socketio.init_app(app, async_mode='eventlet', cors_allowed_origins="*")
-
     register_blueprints(app)
-
     standard_port = 5678
-
-    # fancy box to display the URL
-    url = f" app @:  http://localhost:{standard_port}"
-    box_width = len(url) + 6
-    top_bottom_border = "+" + "-" * (box_width - 2) + "+"
-    spacer = "|" + " " * (box_width - 2) + "|"
-    url_line = "|  " + url + "  |"
-
-    fancy_box = (
-        f"\n\t{top_bottom_border}\n\t{spacer}\n"
-        f"\t{url_line}\n\t{spacer}\n\t{top_bottom_border}"
-    )
-    logger.info(fancy_box)
-
     return app, standard_port
 
 
 def register_blueprints(app):
     from .routes.route_usb import bp as usb_bp
     app.register_blueprint(usb_bp)
-
-    logger.info("Blueprints registered successfully")
--- oats/oats-agent/run.py
+++ oats/oats-agent/run.py
@@ -13,6 +13,20 @@ app, port = create_app()
 
 if __name__ == '__main__':
     try:
+        logger.info("Starting the Flask server")
+        # fancy box to display the URL
+        url = f" app @:  http://localhost:{port}"
+        box_width = len(url) + 6
+        top_bottom_border = "+" + "-" * (box_width - 2) + "+"
+        spacer = "|" + " " * (box_width - 2) + "|"
+        url_line = "|  " + url + "  |"
+
+        fancy_box = (
+            f"\n\t{top_bottom_border}\n\t{spacer}\n"
+            f"\t{url_line}\n\t{spacer}\n\t{top_bottom_border}"
+        )
+        logger.info(fancy_box)
+
         logger.info("Starting the Flask-SocketIO server")
         socketio.run(app, port=port)
         logger.info("Flask-SocketIO server stopped")
